506d71bd1ae857f4477f9515087c820dfccd7b29
TubaMukhtar22 <toobamukhtar121@gmail.com>
2026-01-20 10:29:16 +0500

    Add frontend and backend project

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const app = require("./app");
+const connectDatabase = require("./db/Database");
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
+}
+// connect database
+
+connectDatabase();
+
+// create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for  ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
